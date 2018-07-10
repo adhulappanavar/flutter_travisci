635b584c8ea666193a19bc65557cc16982f36605
Anil Dhulappanavar <anil@Anils-MacBook-Air.local>
2018-07-10 14:33:40 +0530

    Changed the Title

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(title: 'Flutter Travis Demo'),
     );
   }
 }
